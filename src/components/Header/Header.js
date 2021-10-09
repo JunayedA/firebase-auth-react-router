d45922018171a16fd1141ab9a54a9df2ca010d25
JunayedAhmed <junayedahmed11099@gmail.com>
2021-10-09 21:35:48 +0600

    implement loged out button and some func

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
+    const {user} = useFirebase();
     return (
         <div className="header">
             <NavLink to="/home">Home</NavLink>
             <NavLink to="/register">Register</NavLink>
             <NavLink to="/login">Login</NavLink>
-            <button>Log Out</button>
+            {user.email &&<button>Log Out</button>}
         </div>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInUsingGoogle} = useFirebase();
     return (
         <div>
             <h2>Please login</h2>
-            <button>Google sing in</button>
+            <button onClick={signInUsingGoogle}>Google sing in</button>
             <br />
             <NavLink to="/register">New User?</NavLink>
         </div>
